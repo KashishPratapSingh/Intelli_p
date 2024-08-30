fcf80e69950ac3fa1ba78ba1b2afd1131343fdd0
Kashish pratap Singh <singhkashishpratap@gmail.com>
2024-08-30 23:58:44 +0530

    ClassRoom clas of java

---
+++ ClassRoom.java
@@ -0,0 +1,10 @@
+
+public class ClassRoom {
+    public static void main(String[] args) {
+        Object o= new Object();
+        
+    }
+    static void mian(){
+        int result=12;
+    }
+}
